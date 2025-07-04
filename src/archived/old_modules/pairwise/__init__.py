61d61bd407810365b2c881fa4ac2e7eea797d809
DylanJu <jju8@jh.edu>
2025-07-04 12:32:05 -0400

    update module

--- src/archived/old_modules/pairwise/__init__.py
+++
@@ -1,3 +0,0 @@
-# from .all_pairs import rerank as pa_rerank_all
-# from .binary_token_prob import rerank as april_rerank
-from ._prompt_mode import PromptMode
--- src/archived/old_modules/pointwise/__init__.py
+++
@@ -1,2 +0,0 @@
-from .querylikelihood import rerank as qlm_rerank
-from .regression import rerank as pt_rerank
