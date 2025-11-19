46a7e8f0b34f791ced4428c87e3317e43bf5dd1e
DylanJu <j.ju@uva.nl>
2025-11-19 13:14:01 +0100

    add dependency

--- get_bm25_runs.sh
+++
@@ -1,9 +0,0 @@
-# msmarco-passage-v2. trec-dl 2021, 2022, 2023 
-for year in 21 22 23;do
-    python -m pyserini.search.lucene \
-      --threads 16 --batch-size 128 \
-      --index msmarco-v2-passage \
-      --topics dl${year} \
-      --output runs/run.msmarco-passage-v2.bm25.trec-dl-20${year}.txt \
-      --bm25
-done
